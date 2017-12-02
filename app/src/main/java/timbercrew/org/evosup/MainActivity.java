81ac8b5bb766568567ccfafd29b151298493a129
vanilaboy <slawa128128@gmail.com>
2017-12-02 22:22:33 +0300

    sosi zabik

--- app/src/main/java/timbercrew/org/evosup/MainActivity.java
+++ app/src/main/java/timbercrew/org/evosup/MainActivity.java
@@ -6,20 +6,23 @@ import android.os.SystemClock;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Chronometer;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Integer beginTime = 22;
-    private int step;
+    private Integer BEGIN_TIME = 22;
+    private float step;
     private TextView PASSCODE;
     private String chars;
     private Random random;
-    private ProgressBar progressBar;
+    private CircularProgressBar progressBar;
     private Chronometer chronometer;
+    private TextView time;
+    private Integer counterSec = 22;
+    private int elapsedSec = 1000;
+    private int elapsedFull = BEGIN_TIME * 1000;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -31,17 +34,15 @@ public class MainActivity extends AppCompatActivity {
         chronometer = (Chronometer)findViewById(R.id.chronometer2);
         chars = "abcdefghjklmnop";
         chars = chars.toUpperCase();
+        time = (TextView) findViewById(R.id.time);
+        time.setText(counterSec.toString());
         random = new Random(System.currentTimeMillis());
 
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < 5; i++) {
-            res.append(chars.charAt(random.nextInt(chars.length())));
-        }
-        PASSCODE.setText(res.toString());
+        generatePass();
 
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
-        step = progressBar.getMax() / (beginTime) + 1;
-        progressBar.setProgress(0);
+        progressBar = (CircularProgressBar)findViewById(R.id.custom_progressBar);
+        step = 255 / BEGIN_TIME;
+        progressBar.setProgress(-205);
 
         chronometer.setBase(SystemClock.elapsedRealtime());
         chronometer.start();
@@ -53,10 +54,29 @@ public class MainActivity extends AppCompatActivity {
                 long elapsedMillis = SystemClock.elapsedRealtime()
                         - chronometer.getBase();
 
-                if (elapsedMillis > 1000) {
-                    progressBar.setProgress(progressBar.getProgress() + step);
+                if (elapsedMillis > elapsedSec) {
+                    elapsedSec += 1000;
+                    progressBar.setProgressWithAnimation(progressBar.getProgress() + step);
+                    counterSec--;
+                    time.setText(counterSec.toString());
+                }
+                if(elapsedMillis > elapsedFull) {
+                    elapsedFull += (BEGIN_TIME * 1000);
+                    progressBar.setNullWithAnimation();
+                    generatePass();
                 }
             }
         });
+    }
+
+    private void generatePass() {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < 5; i++) {
+            res.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        PASSCODE.setText(res.toString());
+        counterSec = 22;
+        time.setText(counterSec.toString());
+
     }
 }
